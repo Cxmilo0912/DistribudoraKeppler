91e6dad5ae53e151144f7f7e20a680c7777ed8b3
Camilo0912 <julilo09123452@gmail.com>
2026-04-17 12:37:41 -0500

    v0.0_Camilo_Style_Chore: Creacion de estructura basica del proyecto_17/04/2026

---
+++ DistribuidoraKeppler/DistribuidoraKeppler/Datos/ConexionDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DistribuidoraKeppler.Datos
+{
+    public class ConexionDB
+    {
+        private static readonly string cadena = System.Configuration.ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+
+        public static SqlConnection MtAbrirConexion()
+        {
+            if (string.IsNullOrEmpty(cadena))
+            {
+                throw new Exception("La cadena de conexión no se ha configurado correctamente.");
+            }
+
+            return new SqlConnection(cadena);
+        }
+
+    }
+}
