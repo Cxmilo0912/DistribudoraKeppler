08c156847f0d7fb5452d3cdf5744701e83d3b531
Camilo0912 <julilo09123452@gmail.com>
2026-04-20 16:40:39 -0500

    v0.3_Camilo_Style: Se modidfico el index, se agregarron directorios css y js_20/04/2026

--- DistribuidoraKeppler/DistribuidoraKeppler/Index.aspx.designer.cs
+++ DistribuidoraKeppler/DistribuidoraKeppler/Index.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace DistribuidoraKeppler
 
     public partial class Index
     {
-
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
