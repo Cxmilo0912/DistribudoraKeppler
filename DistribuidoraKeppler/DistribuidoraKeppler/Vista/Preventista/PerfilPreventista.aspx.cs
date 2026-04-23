08af7e50670b225c70e2dab215f6880b2e0d6f30
alejandro <alejandrotareas029@gmail.com>
2026-04-23 15:52:29 -0500

    v0.3_Jhon_Chore_Style: Se agrego los aspx de perfil de cada usuario correspondiente_23/04/2026

--- DistribuidoraKeppler/DistribuidoraKeppler/Vista/Preventista/PerfilPreventista.aspx.cs
+++ DistribuidoraKeppler/DistribuidoraKeppler/Vista/Preventista/PerfilPreventista.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace DistribuidoraKeppler.Vista.Cliente
+namespace DistribuidoraKeppler.Vista.Preventista
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class PerfilPreventista : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
