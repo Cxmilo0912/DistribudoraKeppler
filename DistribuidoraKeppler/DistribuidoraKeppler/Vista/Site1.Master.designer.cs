f04ca06e3765da8757352864e6f698f3a1d9d8a3
Andres <castiblancoperezfelipe@gmail.com>
2026-04-23 13:21:02 -0500

    v0.3_Andres_Refactor_Fix: Se corrigio la valdacion del login y la redireccion de los dashboards_23/04/2026

--- DistribuidoraKeppler/DistribuidoraKeppler/Vista/Site1.Master.designer.cs
+++ DistribuidoraKeppler/DistribuidoraKeppler/Vista/Site1.Master.designer.cs
@@ -69,6 +69,15 @@ namespace DistribuidoraKeppler.Vista
         protected global::System.Web.UI.HtmlControls.HtmlAnchor menuPreventista;
 
         /// <summary>
+        /// Control menuRepartidor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor menuRepartidor;
+
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
