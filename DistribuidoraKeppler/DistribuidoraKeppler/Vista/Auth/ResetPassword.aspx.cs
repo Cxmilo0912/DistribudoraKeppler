43643e24510674c793b452e9bf8d4b2fada364a2
Andres <castiblancoperezfelipe@gmail.com>
2026-04-24 17:18:30 -0500

    v0.2_Andres_feat_style_chore:Se hiso nueva funcionalidad para poder recuperar contraseña, se crearon dos aspx y se hizo modificaciones a la clase UsuarioD, tambien se agregaron columnas a las tablas cliente y usuario en la BD_24/04/2026

---
+++ DistribuidoraKeppler/DistribuidoraKeppler/Vista/Auth/ResetPassword.aspx.cs
@@ -0,0 +1,60 @@
+﻿using DistribuidoraKeppler.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DistribuidoraKeppler.Vista.Auth
+{
+    public partial class ResetPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string token = Request.QueryString["token"];
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    Response.Write("Token inválido");
+                    return;
+                }
+
+                UsuarioD datos = new UsuarioD();
+                string email = datos.ObtenerEmailPorToken(token);
+
+                if (email == null)
+                {
+                    Response.Write("Token inválido o no existe");
+                }
+            }
+        }
+        protected void btnCambiar_Click(object sender, EventArgs e)
+        {
+            string token = Request.QueryString["token"];
+
+            UsuarioD datos = new UsuarioD();
+            string email = datos.ObtenerEmailPorToken(token);
+
+            if (email != null)
+            {
+                if (txtNueva.Text == txtConfirmar.Text)
+                {
+                    datos.ActualizarPassword(email, txtNueva.Text);
+
+                    Response.Write("Contraseña actualizada correctamente");
+                }
+                else
+                {
+                    Response.Write("Las contraseñas no coinciden");
+                }
+            }
+            else
+            {
+                Response.Write("Token inválido");
+            }
+        }
+    }
+}
