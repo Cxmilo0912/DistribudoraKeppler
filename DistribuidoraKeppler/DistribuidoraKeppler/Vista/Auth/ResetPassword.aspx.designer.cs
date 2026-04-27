9dbf6d606009a4c9886455f6096c1037d77e1a9e
Andres <castiblancoperezfelipe@gmail.com>
2026-04-27 16:08:50 -0500

    v0.3_Casti_fix_Refactor:Se arreglaron los formatos de recuperarcontraseña y de actualizar contraseña tambien se modifico para que quedara mejor_27/04/2026

--- DistribuidoraKeppler/DistribuidoraKeppler/Vista/Auth/ResetPassword.aspx.designer.cs
+++ DistribuidoraKeppler/DistribuidoraKeppler/Vista/Auth/ResetPassword.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace DistribuidoraKeppler.Vista.Auth
     {
 
         /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
         /// Control txtNueva.
         /// </summary>
         /// <remarks>
