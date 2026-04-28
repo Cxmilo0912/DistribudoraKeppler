c10215d7f6cfff7eea0fe7d50e2e4a63afeb282d
Camilo0912 <julilo09123452@gmail.com>
2026-04-28 12:50:08 -0500

    Camilo_Resolucion de conflicto de merge

--- DistribuidoraKeppler/DistribuidoraKeppler/Vista/Administrador/DashboardAdministrador.aspx.cs
+++ DistribuidoraKeppler/DistribuidoraKeppler/Vista/Administrador/DashboardAdministrador.aspx.cs
@@ -20,19 +20,13 @@ namespace DistribuidoraKeppler.Vista.Aministrador
 
             }
         }
-<<<<<<< HEAD
-        public void CargarTotalClientes() 
-        {
-            int total = clienteD.ObtenerTotalClientes(); // Llama a la base de datos
-            lblTotalClientes.Text = total.ToString();    //Muestra el numero de clientes 
-=======
         public void CargarInfoCards()
         {
             int total = oAdminL.MtTotalClientes();
             lblTotalClientes.Text = total.ToString();
             int totalProductos = oAdminL.MtProductosActivos();
             lblProductosActivos.Text = totalProductos.ToString();
->>>>>>> 89bae78a586bbd7f6702809bcfbbc1ebc218e6f7
+
         }
 
         
